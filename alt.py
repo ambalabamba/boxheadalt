5107c1d4596ae5af8dd3670fd07c44f043065fc9
ambalabamba <38856804+ambalabamba@users.noreply.github.com>
2018-06-01 11:29:47 +0200

    Update alt.py

--- alt.py
+++ alt.py
@@ -122,4 +122,4 @@ class FarmBot:
 
         
 if __name__ == '__main__': # rest in pieces
-    FarmBot('JoynerLucas',  'lolok', '45.63.119.253', 1031)
+    FarmBot('EminenceLikesYoung',  'lolok', '45.63.119.253', 1031)
