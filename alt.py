4b73baab0c3f3d2c2e4d53118866bef78ff9f269
ambalabamba <38856804+ambalabamba@users.noreply.github.com>
2018-06-13 22:23:38 +0200

    Update alt.py

--- alt.py
+++ alt.py
@@ -122,4 +122,4 @@ class FarmBot:
 
         
 if __name__ == '__main__': # rest in pieces
-    FarmBot('EMINENCE.PEDOPHILE',  'lolok', '45.63.119.253', 1031)
+    FarmBot('SluttyWhoreBella..1',  'suckme', '45.63.119.253', 1031)
