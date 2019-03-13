4b48e79d75eed99963ed43071665870afc00f76d
ambalabamba <38856804+ambalabamba@users.noreply.github.com>
2019-03-14 00:37:02 +0100

    Update alt.py

--- alt.py
+++ alt.py
@@ -122,4 +122,4 @@ class FarmBot:
 
         
 if __name__ == '__main__': # rest in pieces
-    FarmBot('YouCantCMyNameBIshMM',  'supercole', '45.63.119.253', 1031)
+    FarmBot('YouCantCMyNameBIshMM',  'Timbo', '45.63.119.253', 1031)
